d903c84d7fb580cc6e7ca63813d0b3103b4539ce
pwwang <pwwang@pwwang.com>
2020-05-11 15:20:27 -0500

    0.0.2

--- pyppl_runcmd.py
+++ pyppl_runcmd.py
@@ -3,7 +3,7 @@ import cmdy
 from pyppl.plugin import hookimpl
 from pyppl.logger import Logger
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 logger = Logger(plugin='runcmd') # pylint: disable=invalid-name
 
